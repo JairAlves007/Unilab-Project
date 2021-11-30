a7f6c126cf2df3553a9732d21cc49adb0cb78c84
Turiel <diogodsap@gmail.com>
2021-11-30 16:43:29 -0300

    Continuamos o desenvolvimento do protótipo da carga horária.

--- resources/views/projects/candidates.blade.php
+++ resources/views/projects/candidates.blade.php
@@ -4,56 +4,57 @@
 
 @section('content')
 
-   @include('layouts.navbar')
+@include('layouts.navbar')
 
-   <div class="d-flex">
+<div class="d-flex">
 
-      @include('layouts.sidebar')
+    @include('layouts.sidebar')
 
-      <div class="content p-1">
-         <div class="list-group-item">
+    <div class="content p-1">
+        <div class="list-group-item">
             <div class="d-flex">
-               <div class="mr-auto p-2">
-                  <h2 class="display-4 titulo">Candidatos</h2>
-               </div>
+                <div class="mr-auto p-2">
+                    <h2 class="display-4 titulo">Candidatos</h2>
+                </div>
             </div>
 
             @include('hintMessages')
 
             <div class="table-responsive">
-               <table class="table table-striped table-hover table-bordered">
-                  <thead>
-                     <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>E-mail</th>
-                        <th class="text-center">Ações</th>
-                     </tr>
-                  </thead>
+                <table class="table table-striped table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                            <th class="text-center">Ações</th>
+                        </tr>
+                    </thead>
 
-                  <tbody>
-                     @foreach ($candidates as $candidate)
+                    <tbody>
+                        @foreach ($candidates as $candidate)
 
                         <tr>
-                           <th>{{ $candidate->user_id }}</th>
-                           <td>{{ $candidate->name }}</td>
-                           <td>{{ $candidate->email}}</td>
-                           <td class="text-center">
-                              <a href="{{ route('projects.approved', $candidate->user_id) }}" class="btn btn-outline-success btn-sm">
-                                 Aprovar
-                              </a>
-                           </td>
+                            <th>{{ $candidate->user_id }}</th>
+                            <td>{{ $candidate->name }}</td>
+                            <td>{{ $candidate->email}}</td>
+                            <td class="text-center">
+                                <a href="{{ route('projects.approved', $candidate->user_id) }}"
+                                    class="btn btn-outline-success btn-sm">
+                                    Aprovar
+                                </a>
+                            </td>
                         </tr>
 
-                     @endforeach
-                  </tbody>
-               </table>
+                        @endforeach
+                    </tbody>
+                </table>
 
             </div>
-         </div>
+        </div>
 
-      </div>
+    </div>
 
-   </div>
+</div>
 
 @endsection
