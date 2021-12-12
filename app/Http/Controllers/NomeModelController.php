211855b21847c13c720760f127073f4e697df793
FGabriel0 <gabrielfabio3g@gmail.com>
2021-12-12 10:52:15 -0300

    Finalizamos o protótipo da frequência do Bolsista

---
+++ app/Http/Controllers/NomeModelController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NomeModel;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NomeModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NomeModel  $nomeModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NomeModel $nomeModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\NomeModel  $nomeModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(NomeModel $nomeModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NomeModel  $nomeModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NomeModel $nomeModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NomeModel  $nomeModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NomeModel $nomeModel)
+    {
+        //
+    }
+}
