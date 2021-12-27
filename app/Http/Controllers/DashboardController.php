2ca4928081906b6e0891e4a8165d3ee1e6caa380
Turiel <diogodsap@gmail.com>
2021-12-27 16:55:44 -0300

    Modificações do funcionamento nde diversos 'layouts' ao longo do site geral.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Edicts;
 use App\Models\User;
 
 class DashboardController extends Controller
 {
-    public function index()
-    {
-        $count_users = count(User::all()->where('id', '<>', auth()->user()->id));
+  public function index()
+  {
+    $count_users = count(User::all()->where('id', '<>', auth()->user()->id));
+    $count_edicts = count(Edicts::all()->where('users_id', '==', auth()->user()->id));
+    $count_all_edicts = count(Edicts::all());
 
-        return view('dashboard', [
-            'count_users' => $count_users
-        ]);
-    }
+    return view('dashboard', [
+      'count_users' => $count_users,
+      'count_edicts' => $count_edicts,
+      'count_all_edicts' => $count_all_edicts
+    ]);
+  }
 }
--- app/Models/Edicts.php
+++ app/Models/Edicts.php
@@ -9,51 +9,52 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Edicts extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+  use HasFactory;
+  use SoftDeletes;
 
-    protected $table = "edicts";
+  protected $table = "edicts";
 
-    protected $fillable = [
-        'edict_title',
-        'archive',
-        'description',
-        'min_titulations_id',
-        'categories_id',
-        'submission_start',
-        'submission_finish',
-        'rate_start',
-        'rate_finish',
-        'execution_start',
-        'execution_finish'
-    ];
+  protected $fillable = [
+    'edict_title',
+    'archive',
+    'description',
+    'min_titulations_id',
+    'categories_id',
+    'submission_start',
+    'submission_finish',
+    'rate_start',
+    'rate_finish',
+    'execution_start',
+    'execution_finish'
+  ];
 
-    protected $dates = [
-        'submission_start',
-        'submission_finish',
-        'rate_start',
-        'rate_finish',
-        'execution_start',
-        'execution_finish',
-        'deleted_at'
-    ];
+  protected $dates = [
+    'submission_start',
+    'submission_finish',
+    'rate_start',
+    'rate_finish',
+    'execution_start',
+    'execution_finish',
+    'deleted_at'
+  ];
 
-    public function ownerEdict() {
-        return $this->hasOne(User::class, 'id', 'users_id');
-    }
+  public function ownerEdict()
+  {
+    return $this->hasOne(User::class, 'id', 'users_id');
+  }
 
-    public function projects() {
-        return $this->hasMany(Projects::class, 'edicts_id', 'id');
-    }
+  public function projects()
+  {
+    return $this->hasMany(Projects::class, 'edicts_id', 'id');
+  }
 
-    public function categories()
-    {
-        return $this->hasOne(Categories::class, 'id', 'categories_id');
-    }
+  public function categories()
+  {
+    return $this->hasOne(Categories::class, 'id', 'categories_id');
+  }
 
-    public function titulations()
-    {
-        return $this->hasOne(MinTitulations::class, 'id', 'min_titulations_id');
-    }
-
+  public function titulations()
+  {
+    return $this->hasOne(MinTitulations::class, 'id', 'min_titulations_id');
+  }
 }
